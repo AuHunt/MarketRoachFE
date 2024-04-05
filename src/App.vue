d18d2152f1347cf05b4ba2b2eb634f2e46fdd89b
Nicolas Ramirez-Diaz <nrma1990@gmail.com>
2024-04-04 23:37:41 -0400

    feat: init with vue and more lightweight

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Market Roach</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
--- src/components/emotion.d.ts
+++
@@ -1,6 +0,0 @@
-import '@emotion/react'
-import { AppTheme } from './App.styled'
-
-declare module '@emotion/react' {
-    export interface Theme extends AppTheme {}
-}
