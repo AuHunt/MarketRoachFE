7350053362e7028327bc73fba4dd350c79bc1448
Nicolas Ramirez-Diaz <nrma1990@gmail.com>
2024-04-26 17:56:34 -0400

    feat: feed scroll vuetify

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,14 @@
             <section class="grid-item">
                 <h1>Market Roach</h1>
             </section>
-            <section class="grid-item tall-grid-item">
-                <h1>Market Roach</h1>
+            <section class="grid-item tall-grid-item" style="overflow-y: scroll">
+                <v-infinite-scroll :items="items" @load="load">
+                    <template v-for="(item, index) in items" :key="item">
+                        <div :class="['pa-2', index % 2 === 0 ? 'bg-grey-lighten-2' : '']">
+                            Item number #{{ item }}
+                        </div>
+                    </template>
+                </v-infinite-scroll>
             </section>
             <section class="grid-item">
                 <h1>Market Roach</h1>
@@ -14,52 +20,57 @@
     </main>
 </template>
 
-<script>
-export default {
-    name: 'App',
+<script setup>
+import { ref } from 'vue'
+
+const items = ref(Array.from({ length: 30 }, (k, v) => v + 1))
+
+async function api() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(Array.from({ length: 10 }, (k, v) => v + items.value.at(-1) + 1))
+        }, 1000)
+    })
+}
+async function load({ done }) {
+    // Perform API call
+    const res = await api()
+
+    items.value.push(...res)
+
+    done('ok')
 }
 </script>
 
 <style lang="scss">
-:root {
-    --background-color: #010113;
-    --text-color: white;
-}
-
 html,
 body {
-    margin: 0;
-    padding: 0;
     height: 100%;
     width: 100%;
 }
 
 body {
-    background-color: var(--background-color);
-    color: var(--text-color);
+    background-color: #010113;
+    color: white;
 }
 
-#app {
+#app,
+.app-container,
+.grid-view {
     height: 100%;
     width: 100%;
     box-sizing: border-box;
 }
 
 .app-container {
-    box-sizing: border-box;
-    width: 100%;
-    height: 100%;
     padding: 10px;
 }
 
 .grid-view {
     display: grid;
-    box-sizing: border-box;
     grid-template-columns: 7fr 3fr;
     grid-template-rows: 7fr 3fr;
     gap: 10px;
-    width: 100%;
-    height: 100%;
 }
 
 .grid-item {
