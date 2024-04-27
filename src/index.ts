0ff7fbf4bd37f2d753c69da2a0cd9f41a7a9cdbd
Nicolas Ramirez-Diaz <nrma1990@gmail.com>
2024-04-27 16:44:22 -0400

    fix: vuetify proper init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
-import { createVuetify } from 'vuetify'
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/lib/styles/main.sass'
 import App from './App.vue'
+import vuetify from './vuetify'
 
-const app = createApp(App)
-const vuetify = createVuetify()
-
-app.use(vuetify)
-app.mount('#app')
+createApp(App).use(vuetify).mount('#app')
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -5,3 +5,5 @@ declare module '*.vue' {
     const component: DefineComponent<{}, {}, any>
     export default component
 }
+declare module 'vuetify/lib/components'
+declare module 'vuetify/lib/directives'
