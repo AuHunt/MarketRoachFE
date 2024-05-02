eaf57d96f5a52b09e1bfb27c427dc05a05e17b52
Nicolas Ramirez-Diaz <nrma1990@gmail.com>
2024-05-02 18:20:53 -0400

    feat: remodeling of ui layout

--- src/App.vue
+++ src/App.vue
@@ -1,191 +1,88 @@
 <template>
-    <v-card class="mx-auto" max-width="400">
-        <v-card-item class="bg-orange-darken-4">
-            <v-card-title> User Directory </v-card-title>
-
-            <template v-slot:append>
-                <v-btn color="white" icon="mdi-plus" size="small"></v-btn>
-            </template>
-        </v-card-item>
-
-        <v-card-text class="pt-4">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi nobis a at voluptates
-            culpa optio amet! Inventore deserunt voluptatem maxime a veniam placeat, eos impedit
-            nulla quos? Officiis, aperiam ducimus.
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-virtual-scroll :items="items" height="300" item-height="50">
-            <template v-slot:default="{ item }">
-                <v-list-item>
-                    <template v-slot:prepend>
-                        <v-avatar :color="item.color" class="text-white" size="40">
-                            {{ item.initials }}
-                        </v-avatar>
+    <v-main width="100%" height="100%">
+        <div class="grid-view">
+            <section class="grid-item">
+                <h1>Market Roach</h1>
+            </section>
+            <section class="grid-item tall-grid-item" style="overflow-y: scroll">
+                <v-infinite-scroll :items="items" @load="load">
+                    <template v-for="(item, index) in items" :key="item">
+                        <div :class="['pa-2', index % 2 === 0 ? 'bg-grey-lighten-2' : '']">
+                            Item number #{{ item }}
+                        </div>
                     </template>
-
-                    <v-list-item-title>{{ item.fullName }}</v-list-item-title>
-
-                    <template v-slot:append>
-                        <v-btn size="small" variant="tonal">
-                            View User
-
-                            <v-icon color="orange-darken-4" end> mdi-open-in-new </v-icon>
-                        </v-btn>
-                    </template>
-                </v-list-item>
-            </template>
-        </v-virtual-scroll>
-    </v-card>
+                </v-infinite-scroll>
+            </section>
+            <section class="grid-item">
+                <h1>Market Roach</h1>
+            </section>
+        </div>
+    </v-main>
 </template>
 
-<script>
-export default {
-    data: () => ({
-        colors: [
-            '#2196F3',
-            '#90CAF9',
-            '#64B5F6',
-            '#42A5F5',
-            '#1E88E5',
-            '#1976D2',
-            '#1565C0',
-            '#0D47A1',
-            '#82B1FF',
-            '#448AFF',
-            '#2979FF',
-            '#2962FF',
-        ],
-        names: [
-            'Oliver',
-            'Jake',
-            'Noah',
-            'James',
-            'Jack',
-            'Connor',
-            'Liam',
-            'John',
-            'Harry',
-            'Callum',
-            'Mason',
-            'Robert',
-            'Jacob',
-            'Jacob',
-            'Jacob',
-            'Michael',
-            'Charlie',
-            'Kyle',
-            'William',
-            'William',
-            'Thomas',
-            'Joe',
-            'Ethan',
-            'David',
-            'George',
-            'Reece',
-            'Michael',
-            'Richard',
-            'Oscar',
-            'Rhys',
-            'Alexander',
-            'Joseph',
-            'James',
-            'Charlie',
-            'James',
-            'Charles',
-            'William',
-            'Damian',
-            'Daniel',
-            'Thomas',
-            'Amelia',
-            'Margaret',
-            'Emma',
-            'Mary',
-            'Olivia',
-            'Samantha',
-            'Olivia',
-            'Patricia',
-            'Isla',
-            'Bethany',
-        ],
-        surnames: [
-            'Smith',
-            'Anderson',
-            'Clark',
-            'Wright',
-            'Mitchell',
-            'Johnson',
-            'Thomas',
-            'Rodriguez',
-            'Lopez',
-            'Perez',
-            'Williams',
-            'Jackson',
-            'Lewis',
-            'Hill',
-            'Roberts',
-            'Jones',
-            'White',
-            'Lee',
-            'Scott',
-            'Turner',
-            'Brown',
-            'Harris',
-            'Walker',
-            'Green',
-            'Phillips',
-            'Davis',
-            'Martin',
-            'Hall',
-            'Adams',
-            'Campbell',
-            'Miller',
-            'Thompson',
-            'Allen',
-            'Baker',
-            'Parker',
-            'Wilson',
-            'Garcia',
-            'Young',
-            'Gonzalez',
-            'Evans',
-            'Moore',
-            'Martinez',
-            'Hernandez',
-            'Nelson',
-            'Edwards',
-            'Taylor',
-            'Robinson',
-            'King',
-            'Carter',
-            'Collins',
-        ],
-    }),
+<script setup>
+import { ref } from 'vue'
 
-    computed: {
-        items() {
-            const namesLength = this.names.length
-            const surnamesLength = this.surnames.length
-            const colorsLength = this.colors.length
+const items = ref(Array.from({ length: 30 }, (k, v) => v + 1))
 
-            return Array.from({ length: 10000 }, () => {
-                const name = this.names[this.genRandomIndex(namesLength)]
-                const surname = this.surnames[this.genRandomIndex(surnamesLength)]
+async function api() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(Array.from({ length: 10 }, (k, v) => v + items.value.at(-1) + 1))
+        }, 1000)
+    })
+}
+async function load({ done }) {
+    // Perform API call
+    const res = await api()
 
-                return {
-                    color: this.colors[this.genRandomIndex(colorsLength)],
-                    fullName: `${name} ${surname}`,
-                    initials: `${name[0]} ${surname[0]}`,
-                }
-            })
-        },
-    },
+    items.value.push(...res)
 
-    methods: {
-        genRandomIndex(length) {
-            return Math.ceil(Math.random() * (length - 1))
-        },
-    },
+    done('ok')
 }
 </script>
+
+<style lang="scss">
+html,
+body {
+    height: 100%;
+    width: 100%;
+}
+
+body {
+    background-color: #010113;
+    color: white;
+}
+
+#app,
+.app-container,
+.grid-view {
+    height: 100%;
+    width: 100%;
+    box-sizing: border-box;
+}
+
+.app-container {
+    padding: 10px;
+}
+
+.grid-view {
+    display: grid;
+    grid-template-columns: 7fr 3fr;
+    grid-template-rows: 7fr 3fr;
+    gap: 10px;
+}
+
+.grid-item {
+    border-radius: 5px;
+    border: 1px solid white;
+}
+
+.tall-grid-item {
+    grid-row: span 2;
+}
+
+h1 {
+    color: #42b983;
+}
+</style>
